a76ad3c1bc778fd72be14f7c423394d865e5f020
Ankitakumari29 <111568046+Ankitakumari29@users.noreply.github.com>
2022-12-08 17:30:07 +0530

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,12 @@
-/*
-Here all the actions are defined.
-Example of defining an actoin is as follows
-export const repairAdded = (var1,va2) => {
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export const loginAction = (username)=> {
   return {
-    type: "actionType1",
-    payload: {
-      var1,
-      var2
-    }
+    type : LOGIN,
+    payload : username,
   }
 }
-*/
+export const logoutAction = ()=>{
+  return {type:LOGOUT}
+}
